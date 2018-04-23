2ce1fdc474809a490812b56896eecb912e156522
Robert Prince <rob23codingbootcamp@gmail,com>
2018-04-23 17:04:56 -0400

    fixed repo

--- bamazon_customer.js
+++ bamazon_customer.js
@@ -19,30 +19,67 @@ connection.connect(function(err){
 
 function start(){
  
-    function readProducts();
-    inquirer
-    .prompt({
-        name: "idAndQuantity",
-        type: "list",
-        message: "What is the id of the product that you would like to purchase? How many itmes would you like to buy?",
-        choices: ["id","stockQuantity"]
-})
-.then(function(answer){
-
-})
-
-
-
-}
-
 function readProducts(){
     console.log("selecting all products");
     connection.query("SELECT * FROM products", function(err, res ) {
     if  (err) throw err;    
     console.log(res);
-    connection.end();
+
     });
-}
+
+    function startBamazon(){
+
+        inquirer
+        .prompt({
+            name: "idAndPrice",
+            type: "input",
+            message: "Please enter the [id] of the product that you would like to purchase",
+            choices: ["id"]
+        })
+        .then(function(answer){
+            productId = answer.idAndPrice;
+
+
+        });
+
+        inquirer
+        .prompt(
+           {
+               name: "idAndPrice",
+               type: "input",
+               message: "What [quantity] of this item would you like to purchase",
+               choices: ["quantity"]
+    })
+
+       .then(function(answer){
+           productQuantity = answer.idAndPrice;
+       });
+
+
+
+function getBamazonId(productId, productQuantity){
+    connection.query("SELECT * FROM bamazon_db", function(err, results){
+        if (err) throw err;
+        inquirer
+        .prompt({
+         name: "choice",
+         type: "list",
+         choices: function(){
+             for (var i = 0; i < results.length; i++){
+                 if (results[i].id === productId){
+                     bamzonStkQuantity = results[i].stockQuantity;
+
+                 }
+             }
+         }
+        })
+        if (productQuantity > bamazonStkQuantity){
+            console.log("Sorry.. we have an insufficient quantity of this item to fill your order");
+        } else{
+            bamazonStkQuantity = bamazonStkQuantity - productQuantity;
+            fillOrder(bamazonStkQuantity);
+        }
+
 
 
 
